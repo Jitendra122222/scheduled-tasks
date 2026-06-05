b085ab0e35b2eac19651519f7530691d7ac4a6fe
Jitendra122222 <32477994+Jitendra122222@users.noreply.github.com>
2026-06-05 22:44:41 +0530

    Integrate weather API and send WhatsApp alertsrain alert script
    Replaced email notification with weather-based WhatsApp message.

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+weather_params = {
+  "lat": "43.343777",
+  "lon": "-0.127758",
+  "appid": api_key,
+  "cnt": 4,
+}
+response = requests.get(OWM_Endpoint, params=weather_params)
+weather_data = response.json()
+will_snow = False
+for hour_data in weather_data["list"]:
+  condition_code = hour_data["weather"][0]["id"]
+  if int(condition_code) < 900:
+    will_snow = True
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+if will_snow:
+  client = Client(account_sid, auth_token)
+  print(client)
+  try:
+    message = client.messages.create(
+      from_="whatsapp:+14155238886",
+      body="It's going to rain today. Remember to bring an umbrella",
+      to="whatsapp:+918208134214"
+    )
+    print(message.body)
+  except Exception as e:
+    print("Failed to send WhatsApp message:", e)
+else:
+  print("No snow expected in the next forecast window.")
